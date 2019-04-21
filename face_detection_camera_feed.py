af6145b7f1ac5f6f845291eaba7b1eec88f7e057
Brian Oppenheim <brian_oppenheim@brown.edu>
2019-04-21 17:23:44 -0400

    couple changes

--- face_detection_camera_feed.py
+++ face_detection_camera_feed.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 from openface import load_openface
 import mlp
+from skimage.transform import resize
 faceCascade = cv2.CascadeClassifier("/Users/Eleanor/Desktop/CryptoBeat Videos/haarcascade_frontalface_default.xml")
 
 video_capture = cv2.VideoCapture(0)
@@ -17,11 +18,11 @@ while True:
     #ret is error code but we don't care about it
     ret, frame = video_capture.read()
 
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Trained on the haarcascade_frontalface_default dataset
     faces = faceCascade.detectMultiScale(
-        gray_frame,
+        frame,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
@@ -30,11 +31,11 @@ while True:
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    faceCrop = frame[x:x+w, y:y+h]
-    #Reshape facecrop
-    latentFaceVector = openFace(faceCrop)
-    result = classifier(latentFaceVector) #TODO uncomment when we get classifier.
-    #TODO: Tag the frame with 
+        faceCrop = frame[x:x+w, y:y+h]
+        faceCrop = resize(faceCrop, (96, 96), anti_aliasing-True)
+        latentFaceVector = openFace(faceCrop)
+        result = classifier(latentFaceVector) #TODO uncomment when we get classifier.
+        #TODO: Tag the frame with facerino
     cv2.imshow('Camera Feed', frame)
 
 # When everything is done, release the capture
