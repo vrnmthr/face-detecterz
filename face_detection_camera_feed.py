f32bee25e038a0e663fd27aca28ed38ecf2f55f4
Brian Oppenheim <brian_oppenheim@brown.edu>
2019-04-23 14:39:52 -0400

    halfway done with repl

--- face_detection_camera_feed.py
+++ face_detection_camera_feed.py
@@ -4,6 +4,7 @@ from openface import load_openface
 import mlp
 from skimage.transform import resize
 import torch.nn
+import time
 faceCascade = cv2.CascadeClassifier("/Users/Eleanor/Desktop/CryptoBeat Videos/haarcascade_frontalface_default.xml")
 video_capture = cv2.VideoCapture(0)
 device = ""
@@ -13,8 +14,30 @@ else:
     device = "cpu"
 openFace = load_openface(device)
 classifier = mlp.load_mlp(device, 3)
-      
-if __name__ == '__main__':
+
+def promptFaceTraining(seconds):
+    print("Capture starting in 5 seconds. Bring your face 2 feet from camera and slowly rotate!")
+    time.sleep(5)
+    print("Capturing! about to capture" + seconds + " seconds of video")
+    time = time.now()
+    while time.now() -time < seconds:
+         ret, frame = video_capture.read()
+         # Trained on the haarcascade_frontalface_default dataset
+         faces = faceCascade.detectMultiScale(
+            frame,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            faceCrop = frame[x:x+w, y:y+h]
+            faceCrop = resize(faceCrop, (96, 96), anti_aliasing=True)
+            latentFaceVector = openFace(faceCrop)
+            #TODO: ahhh save theFACE to disk somewhere!!
+            
     while True:
 
         #ret is error code but we don't care about it
