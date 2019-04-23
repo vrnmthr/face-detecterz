d8a20b78d05363149f28efa758f64e86b8170459
Eleanor Avril <eleanor_avril@brown.edu>
2019-04-23 00:32:22 -0400

    fixing align_faces.py

--- align_faces.py
+++ align_faces.py
@@ -8,8 +8,10 @@ from imutils import face_utils
 # below is code to align one image
 
 FACE_SIZE = 256
-LEFT_EYE_LOC = (.35, .35)
-RIGHT_EYE_LOC = (.65, .65)
+#outside corners
+LEFT_EYE_LOC = (0.252418718401, 0.331052263829)
+RIGHT_EYE_LOC = (0.782865376271, 0.321305281656)
+NOSE_LOC = (0.520712933176, 0.634268222208)
 GOAL_DIST = 76.8
 
 detector = dlib.get_frontal_face_detector()
@@ -37,13 +39,14 @@ def align_and_extract_faces(img, test=False):
 
         points = predictor(gray, f)
         points = face_utils.shape_to_np(points)
-        l1 = points[0]
-        l2 = points[1]
-        r1 = points[2]
-        r2 = points[3]
+        l_outside = points[0]
+        l_inside = points[1]
+        r_inside = points[2]
+        r_outside = points[3]
+        nose = points[4]
 
-        left_eye_center = [(l1[0] + l2[0]) / 2, (l1[1] + l2[1]) / 2]
-        right_eye_center = [(r1[0] + r2[0]) / 2, (r1[1] + r2[1]) / 2]
+        left_eye_center = [(l_outside[0] + l_inside[0]) / 2, (l_outside[1] + l_inside[1]) / 2]
+        right_eye_center = [(r_inside[0] + r_outside[0]) / 2, (r_inside[1] + r_outside[1]) / 2]
 
         dY = right_eye_center[1] - left_eye_center[1]
         dX = right_eye_center[0] - left_eye_center[0]
@@ -55,12 +58,8 @@ def align_and_extract_faces(img, test=False):
         dist = np.sqrt(dX ** 2 + dY ** 2)
         scale = GOAL_DIST / dist
 
-        M = cv2.getRotationMatrix2D(eyes_center, angle_between_eyes, scale)
-        tX = FACE_SIZE * .5
-        tY = FACE_SIZE * LEFT_EYE_LOC[1]
-        M[0, 2] += (tX - eyes_center[0])
-        M[1, 2] += (tY - eyes_center[1])
-
+        M = cv2.getAffineTransform([l_outside, r_outside, nose], [LEFT_EYE_LOC, RIGHT_EYE_LOC, NOSE_LOC])
+        
         out = cv2.warpAffine(img, M, (FACE_SIZE, FACE_SIZE), flags=cv2.INTER_CUBIC)
 
         if test:
