a2eb38b96c24743db0ac77691535ee37576a1967
Brian Oppenheim <brian_oppenheim@brown.edu>
2019-04-30 16:10:53 -0400

    pulling changes to work on repl

--- setup.sh
+++ setup.sh
@@ -11,14 +11,12 @@ cd distro
 bash install-deps
 ./install.sh
 # add to path and add dependencies
-echo 'export PATH=$PATH:~/Source/torch/distro/install/bin' >> ~/.bash_profile
+echo 'export PATH=$PATH:~/Source/torch/distro/install/bin' >> ~/.bash_rc
 source ~/.bash_profile
 for NAME in dpnn nn optim csvigo cutorch cunn; do luarocks install $NAME; done
 
 # install openface to ~/Source/openface
 cd ~/Source/
-brew install findutils
-brew install coreutils
 git clone https://github.com/cmusatyalab/openface.git --recursive
 cd openface
 python2 setup.py install
