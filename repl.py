c0421b1526495864d24ba7457d4103a130728111
Varun Mathur <varun.mathur@live.com>
2019-04-30 20:29:28 -0400

    Made frame capture method work correctly

--- repl.py
+++ repl.py
@@ -1,19 +1,13 @@
-import cv2
-#import mlp
-import torch.nn
-<<<<<<< HEAD
-from align_faces import align_and_extract_faces
-from openface import load_openface, preprocess_single, preprocess_batch
-import numpy as np
 import time
-=======
-from align_faces.py import align_and_extract_faces
-from openface import load_openface, preprocess_single
-from sklearn import svm
-from classifier import train, test
->>>>>>> 3874d60a042aa29ebf9eaa6ccfdc772ebea93d67
 
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+import cv2
+import numpy as np
+import torch.nn
+
+from align_faces import extract_faces, align_faces
+from openface import load_openface, preprocess_single, preprocess_batch
+from tqdm import tqdm
+
 video_capture = cv2.VideoCapture(0)
 device = ""
 if torch.cuda.is_available():
@@ -21,48 +15,53 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 openFace = load_openface(device)
-clf = svm.SVC(kernel="linear", C=1.6)
+# clf = svm.SVC(kernel="linear", C=1.6)
 
 CONF_THRESHOLD = .5
 
-idxToName = {} #TODO: Write function to populate.
+idxToName = {}  # TODO: Write function to populate.
 
-def promptFaceTraining(seconds):
-    print("Capture starting in 5 seconds. Bring your face 2 feet from camera and slowly rotate!")
-    time.sleep(5)
-    print("Capturing! about to capture " + str(seconds) + " seconds of video")
-    t = time.time()
-    samples = []
-    count = 0
-    while time.time() - t < seconds:
+
+def capture_faces(seconds=5, sampling_duration=0.1):
+    print("Capturing! about to capture {} seconds of video".format(seconds))
+    start_time = time.time()
+
+    # face_locs stores the bounding box coordinates
+    face_locs = []
+    # frames stores the actual images
+    frames = []
+
+    while time.time() - start_time < seconds:
         ret, frame = video_capture.read()
-        # Trained on the haarcascade_frontalface_default dataset
-        faces = faceCascade.detectMultiScale(
-            frame,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-        if (len(faces) == 1): #yeet
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                faceCrop = frame[y:y + h, x:x + w]
-                cv2.imshow("yeet1", faceCrop)
-                cv2.waitKey(0)
-                faceCrop = align_and_extract_faces(faceCrop)[0]
-                samples.append(faceCrop)
-                cv2.imshow("yeet2", faceCrop)
-                cv2.waitKey(0)
-                cv2.imwrite("test_faces/eleanor" + str(count) + ".png", np.float32(faceCrop))
-                count += 1
-        else: #yote
-            print("We have found " + str(len(faces)) + ", and there should only be one. Try again.")
-    # batch
-    faces_proc = preprocess_batch(samples)
-    latentFaceVectors = openFace(faces_proc)
-    np.save("test_faces/eleanor.npy", latentFaceVectors)
+        if ret:
+            faces = extract_faces(frame)
+            if len(faces) == 1:
+                frames.append(frame)
+                face_locs.append(faces[0])
+                print("Taken sample.")
 
+            if len(faces) == 0:
+                print("No faces found.")
+
+            if len(faces) > 1:
+                print("We have found {} faces, and there should only be one".format(len(faces)))
+        else:
+            print("ERROR: No sample taken")
+        # lock the loop to system time
+        time.sleep(sampling_duration - ((time.time() - start_time) % sampling_duration))
+
+    # extract the faces afterwards
+    print("Extracting faces from samples")
+    samples = []
+    for i in tqdm(range(len(face_locs)), total=len(face_locs)):
+        rect = face_locs[i]
+        frame = frames[i]
+        sample = align_faces(frame, [rect])[0]
+        samples.append(sample)
+        cv2.imshow("samples", sample)
+        cv2.waitKey(0)
+
+    return samples
 
 
 def main():
@@ -76,7 +75,7 @@ def main():
         # cv2.imshow("wow", frame)
         # cv2.waitKey(0)
 
-        #Trained on the haarcascade_frontalface_default dataset
+        # Trained on the haarcascade_frontalface_default dataset
         faces = faceCascade.detectMultiScale(
             frame,
             scaleFactor=1.1,
@@ -96,10 +95,11 @@ def main():
             print(pred)
             # softmax = nn.Softmax(result)
             # classPredicted = np.argmax(softmax)
-            #confidence = softmax[classPredicted]
-            #prev_conf[conf_idx] = confidence
+            # confidence = softmax[classPredicted]
+            # prev_conf[conf_idx] = confidence
             conf_idx += 1
-            if np.sum(prev_conf) / CONF_TO_STORE < CONF_THRESHOLD:  # TODO: Create heuristic for confidence and track frame history.
+            if np.sum(
+                    prev_conf) / CONF_TO_STORE < CONF_THRESHOLD:  # TODO: Create heuristic for confidence and track frame history.
                 print("We don't recognize you!")
                 promptFaceTraining(5)
             # TODO: Tag the frame with facerino -- get the name somehow
@@ -113,4 +113,5 @@ def main():
     cv2.destroyAllWindows()
 
 
-if __name__ == "__main__": promptFaceTraining(5)
+if __name__ == "__main__":
+    capture_faces(seconds=10, sampling_duration=0.25)
